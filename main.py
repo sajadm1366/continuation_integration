0a3da1f7942596907866a75221e582b7c87c1c88
sajad <mehrizis@gmail.com>
2022-05-26 21:27:55 +0200

    add more functions

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 A simple program
 '''
 
-def print_hi(name):
+# import libraries
+import numpy as np
 
-    print(f'Hi, {name}')
+def add_fucntion(x, y):
+    return x + y
 
-# Press the green button in the gutter to run the script.
+def multiply_function(x, y):
+    return x * y
+
+def main():
+    x = 2
+    y = 3
+    print(f'2 plus 3 is: {add_fucntion(2, 3)}')
+    print(f'2 multiply 3 is: {multiply_function(2, 3)}')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 
